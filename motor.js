ced1515071b27176f69b94e2acaa72705cc56760
amirpasic <146943460+amirpasic@users.noreply.github.com>
2023-10-04 17:39:39 +0200

    Update motor.js

--- motor.js
+++ motor.js
@@ -1,1 +1,24 @@
-alert("Ihr Text hier");
+const pageStatus = 0;
+
+if (pageStatus === 1) {
+  const overlay = document.querySelector('div'); // Finde das Overlay-Element
+  if (overlay) {
+    overlay.remove(); // Entferne das Overlay
+  }
+  document.body.style.overflow = 'auto'; // Erlaube das Scrollen auf der Seite
+} else {
+  const overlay = document.createElement('div');
+  overlay.style.position = 'fixed';
+  overlay.style.top = '0';
+  overlay.style.left = '0';
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+  overlay.style.background = 'gray';
+  document.body.appendChild(overlay);
+  document.body.style.overflow = 'hidden';
+}
+
+// Verhindert das Caching der Seite
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+});
