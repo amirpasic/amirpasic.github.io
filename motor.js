2616af3a480014fd21f02a258ec149f36b2d69e8
amirpasic <146943460+amirpasic@users.noreply.github.com>
2023-10-04 17:37:07 +0200

    Update motor.js

--- motor.js
+++ motor.js
@@ -1,12 +1,1 @@
-const pageStatus = 0;
-
-  const overlay = document.createElement('div');
-  overlay.style.position = 'fixed';
-  overlay.style.top = '0';
-  overlay.style.left = '0';
-  overlay.style.width = '100%';
-  overlay.style.height = '100%';
-  overlay.style.background = 'gray';
-  document.body.appendChild(overlay);
-  document.body.style.overflow = 'hidden';
-
+alert("Ihr Text hier");
