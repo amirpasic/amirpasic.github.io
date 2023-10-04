68f3b5c1ab4b47331894fa7deefdf72f3d07b2a7
amirpasic <146943460+amirpasic@users.noreply.github.com>
2023-10-04 17:34:38 +0200

    Update motor.js

--- motor.js
+++ motor.js
@@ -1,16 +1,5 @@
 const pageStatus = 0;
 
-window.addEventListener('beforeunload', function (e) {
-    e.preventDefault();
-});
-
-if (pageStatus === 1) {
-  const overlay = document.querySelector('div'); // Finde das Overlay-Element
-  if (overlay) {
-    overlay.remove(); // Entferne das Overlay
-  }
-  document.body.style.overflow = 'auto'; // Erlaube das Scrollen auf der Seite
-} else {
   const overlay = document.createElement('div');
   overlay.style.position = 'fixed';
   overlay.style.top = '0';
@@ -20,4 +9,4 @@ if (pageStatus === 1) {
   overlay.style.background = 'gray';
   document.body.appendChild(overlay);
   document.body.style.overflow = 'hidden';
-}
+
